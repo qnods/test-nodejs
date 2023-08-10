95d0f9aa5f8c4a45c3318f3711a2c106440b489b
Edward <edward@qnods.com>
2023-08-10 15:31:30 +0000

    update async section

---
+++ node-basic/test.js
@@ -0,0 +1,8 @@
+const name = "Pikachu";
+
+const greet = (pkmn_name) => {
+    console.log(`hello, ${pkmn_name}`);
+}
+
+greet("bulbasaur")
+greet("squirtle")
--- node-basic/time.js
+++ node-basic/time.js
@@ -1,10 +1,12 @@
-//
+// run console log after 3 sec
 
 setTimeout(() => {
    console.log("in the timeout");
-   clearInterval(int);
+   clearInterval(int); // terminate interval function below
 }, 3000);
 
+
+// run console log every 1 second
 const int = setInterval(() => {
     console.log("interval running")
 }, 1000);
